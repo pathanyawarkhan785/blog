6945403029759b2e433de3e3b407a2ec75beb2be
yawar5 <yawar@socialpilot.co>
2022-03-03 18:20:39 +0530

    commited.

--- test/index.test.js
+++ test/index.test.js
@@ -4,77 +4,198 @@ const router = require("../index");
 const request = supertest.agent(router.callback());
 
 describe("registeration", () => {
-  // beforeEach(() => {
-  //   console.log(1 + 1);
-  // });
+  test("if registeration details null.", async () => {
+    const registerData = await request.post("/register").send();
 
-  test("if register details null.", async () => {
-    const emailData = await request.post("/register").send();
-    expect(emailData.body).toEqual({
+    expect(registerData.body).toEqual({
       msg: "email is compulsory,string & not null.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if only send email in body.", async () => {
+    query.findEmail = jest.fn();
+
+    const emailData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(emailData.body).toEqual({
+      msg: "firstName is compulsory,not null and in string.",
     });
     expect(emailData.status).toEqual(400);
   });
 
-  // afterEach(() => {
-  //   console.log(2 + 2);
-  // });
+  test("if only send firstName in body.", async () => {
+    const firstNameData = await request.post("/register").send({
+      firstName: "rajdeep",
+    });
 
-  test("throw error if email in database.", async () => {
-    query.findEmail = jest.fn(() => ({
-      email: "ywr@gmail.com",
-    }));
+    expect(firstNameData.body).toEqual({
+      msg: "email is compulsory,string & not null.",
+    });
+    expect(firstNameData.status).toEqual(400);
+  });
 
-    const emailData = await request.post("/register").send({
-      email: "ywr@gmail.com",
-      firstName: "rajdeepsinh",
+  test("if only send password in body.", async () => {
+    const passwordData = await request.post("/register").send({
+      password: "rd123",
+    });
+
+    expect(passwordData.body).toEqual({
+      msg: "email is compulsory,string & not null.",
+    });
+    expect(passwordData.status).toEqual(400);
+  });
+
+  test("if only send email and firstName in body.", async () => {
+    query.findEmail = jest.fn();
+
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+      firstName: "rajdeep",
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(registerData.body).toEqual({
+      msg: "password is compulsory,not null and in string.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if only send email and password in body.", async () => {
+    query.findEmail = jest.fn();
+
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
       password: "rd123",
     });
 
     expect(query.findEmail).toBeCalledTimes(1);
-    expect(emailData.status).toEqual(400);
-    expect(emailData.body).toEqual({ msg: "email must be unique." });
+    expect(registerData.body).toEqual({
+      msg: "firstName is compulsory,not null and in string.",
+    });
+    expect(registerData.status).toEqual(400);
   });
 
-  test("correct registration details.", async () => {
+  test("if only send firstName and password in body.", async () => {
+    const registerData = await request.post("/register").send({
+      firstName: "rajdeep",
+      password: "rd123",
+    });
+
+    expect(registerData.body).toEqual({
+      msg: "email is compulsory,string & not null.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if email exists in database", async () => {
     query.findEmail = jest.fn(() => ({
-      email: "rajdeep@gmail.com",
+      msg: "email must be unique.",
     }));
+    const emailData = await request.post("/register").send({
+      email: "ywr@gmail.com",
+    });
 
-    query.registerData = jest.fn(() => ({}));
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(emailData.body).toEqual({ msg: "email must be unique." });
+    expect(emailData.status).toEqual(400);
+  });
 
-    const registerDetails = await request.post("/register").send({
+  test("if email not in correct format.", async () => {
+    const registerData = await request.post("/register").send({
+      email: "rajdeepgmailc.o2m",
+    });
+
+    expect(registerData.body).toEqual({
+      msg: "enter email in correct format.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if password length less than 5.", async () => {
+    query.findEmail = jest.fn();
+
+    const password = await request.post("/register").send({
       email: "rajdeep@gmail.com",
-      firstName: "rajdeepsinh",
+      firstName: "rajdeep",
+      password: "rd12",
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(password.body).toEqual({
+      msg: "password length must be atleast 5.",
+    });
+    expect(password.status).toEqual(400);
+  });
+
+  test("if lastname is in blank string.", async () => {
+    query.findEmail = jest.fn();
+
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+      firstName: "rajdeep",
+      password: "rd123",
+      lastName: "     ",
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(registerData.body).toEqual({
+      msg: "lastName can not be empty.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+  test("if mobileno is not in string.", async () => {
+    query.findEmail = jest.fn();
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+      firstName: "rajdeep",
+      password: "rd123",
+      mobileno: 9087654321,
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(registerData.body).toEqual({
+      msg: "mobileno must be in string.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if mobileno is not in correct format.", async () => {
+    query.findEmail = jest.fn();
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+      firstName: "rajdeep",
+      password: "rd123",
+      mobileno: "727278",
+    });
+
+    expect(query.findEmail).toBeCalledTimes(1);
+    expect(registerData.body).toEqual({
+      msg: "enter mobile number in correct format.",
+    });
+    expect(registerData.status).toEqual(400);
+  });
+
+  test("if mobileno is exists in database.", async () => {
+    query.findEmail = jest.fn();
+    query.findMobileno = jest.fn(() => ({
+      msg: "mobileno must be unique.",
+    }));
+    const registerData = await request.post("/register").send({
+      email: "rajdeep@gmail.com",
+      firstName: "rajdeep",
       password: "rd123",
       mobileno: "8487864415",
     });
 
     expect(query.findEmail).toBeCalledTimes(1);
-    // expect(registerDetails.status).toEqual(201);
-    expect(registerDetails.body).toEqual(1);
-  });
-});
-
-describe("check login", () => {
-  test("login null", async () => {
-    const data = await request.post("/login").send();
-
-    expect(data.body).toEqual({ msg: "user not found." });
-  });
-
-  test("valid login credentials", async () => {
-    query.findEmail = jest.fn(() => ({
-      email: "ywr@gmail.com",
-      password: "ywr123",
-    }));
-
-    const result = await request
-      .post("/login")
-      .send({ email: "ywr@gmail.com", password: "ywr123" });
-
-    expect(query.findEmail).toBeCalledTimes(1);
-    // expect(result.body).toBe(1);
-    expect(result.status).toEqual(200);
+    expect(query.findMobileno).toBeCalledTimes(1);
+    expect(registerData.body).toEqual({
+      msg: "mobileno must be unique.",
+    });
+    expect(registerData.status).toEqual(400);
   });
 });
